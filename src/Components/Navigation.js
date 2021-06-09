8c4c7d0395ea3d9110411621eab153d828d543f4
yogeshwari-jhala <yogeshwarijhala1627@gmail.com>
2021-06-09 16:09:45 +0530

    Login Page Done

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
 import { FaUsers } from 'react-icons/fa';
+import { Link } from '@reach/router';
 class Navigation extends Component {
   render() {
-    const { user } = this.props;
+    const { user, logOutUser } = this.props;
 
     return (
       <nav className="site-nav family-sans navbar navbar-expand bg-primary navbar-dark higher">
         <div className="container-fluid">
-          <a href="/" className="navbar-brand">
+          <Link to="/" className="navbar-brand">
             <FaUsers className="mr-1" /> Meeting Log
-          </a>
+          </Link>
           <div className="navbar-nav ml-auto">
             {user && (
-              <a className="nav-item nav-link" href="/meetings">
+              <Link className="nav-item nav-link" to="/meetings">
                 meetings
-              </a>
+              </Link>
             )}
             {!user && (
-              <a className="nav-item nav-link" href="/login">
+              <Link className="nav-item nav-link" to="/login" onClick={e => logOutUser(e)}>
                 log in
-              </a>
+              </Link>
             )}
             {!user && (
-              <a className="nav-item nav-link" href="/register">
+              <Link className="nav-item nav-link" to="/register">
                 register
-              </a>
+              </Link>
             )}
             {user && (
-              <a className="nav-item nav-link" href="/login">
+              <Link className="nav-item nav-link" to="/login">
                 log out
-              </a>
+              </Link>
             )}
           </div>
         </div>
